f6b6e9d036e52941bc05873a9f8cc03f573b33db
zhangguorui <zhangguorui@100tal.com>
2022-11-18 22:31:45 +0800

    scoped

--- src/components/BaseCom.vue
+++ src/components/BaseCom.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
         <span @click="say">{{msg}}</span>
-        <span>{{ name }}</span>
+        <span class="name">{{ name }}</span>
         <span>{{ age+1 }}</span>
         <span>{{ obj.color }}</span>
         <span>{{ obj.size }}</span>
+        <SonComponent/>
     </div>
 </template>
 <script>
  import { myMixin, myMixin2 } from './mxin';
-
+ import SonComponent from './SonComponent.vue';
     export default{
         name:"BaseCom",
         props:['name','age','obj'],
+        components:{SonComponent},
         data(){
             return {
                 msg:"ax"
@@ -28,8 +30,10 @@
         mixins:[myMixin,myMixin2],
     }
 </script>
-
-<style>
-
+    
+<style >
+    .name{
+        background-color: rgb(138, 138, 221);
+    }
 
 </style>
---
+++ src/components/SonComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button class="name">点我</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'SonComponent',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+  .name{
+    background-color: yellow;
+  }
+</style>
