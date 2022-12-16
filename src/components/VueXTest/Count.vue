1da4eed768fc8ca9d22fec8e3575265c8d988d3e
zhangguorui <zhangguorui>
2022-12-16 17:48:25 +0800

    mapActions和mapMutations使用 方法要记得传值，用数组形式的时候要记得改名字

--- src/components/VueXTest/Count.vue
+++ src/components/VueXTest/Count.vue
@@ -8,15 +8,21 @@
         <option value="2">2</option>
         <option value="3">3</option>
     </select>
-    <button @click="addClick">+</button>
-    <button @click="reduceClick">—</button>
-    <button @click="sigleClick">基数才加</button>
-    <button @click="waitClick">等下再加</button>
+    <!-- 如果使用mapActions和mapMutations的话要传递参数不然拿到的是默认的$event -->
+    <button @click="addClick(tempNum)">+</button>
+    <button @click="reduceClick(tempNum)">—</button>
+    <button @click="sigleClick(tempNum)">基数才加</button>
+    <button @click="waitClick(tempNum)">等下再加</button>
+    <!-- 用数组的时候名字用下边的 -->
+    <!-- <button @click="ADD(tempNum)">+</button> -->
+    <!-- <button @click="REDUCE(tempNum)">—</button> -->
+    <!-- <button @click="addOdd(tempNum)">基数才加</button> -->
+    <!-- <button @click="addWait(tempNum)">等下再加</button> -->
   </div>
 </template>
 
 <script>
-import { mapState,mapGetters } from 'vuex';
+import { mapState,mapGetters,mapActions,mapMutations} from 'vuex';
 export default {
     name:"Count",
     data() {
@@ -40,13 +46,13 @@ export default {
             return this.$store.getters.bigSum;
         }, */
         //用mapstate从state中读取数据...解构赋值 computed中写入对象的每一个键值对
-        ...mapState({sum:'sum',school:'school',name:'name'}),//对象写法
+        ...mapState({sum:'sum',school:'school',name:'name'}),//对象写法 不能使用简写形式{sum,school}因为后边的是字符串不是对象
         // ...mapState(['sum','school','name'])//数组写法
         // ...mapGetters({bigSum:"bigSum"}),//对象写法
         ...mapGetters(['bigSum'])//数组写法
     },
     methods: {
-        addClick(){
+       /*  addClick(){
             // this.sum += this.tempNum;
             // this.$store.dispatch('add',this.tempNum);
             this.$store.commit('ADD',this.tempNum);
@@ -68,7 +74,13 @@ export default {
                 // this.sum += this.tempNum;
                 // }, 500);
             this.$store.dispatch('addWait',this.tempNum);
-        },
+        },*/ 
+        //借助mapActions生产对应的方法，方法中会调用dispatch去联系actions
+        ...mapActions({sigleClick:'addOdd',waitClick:'addWait'}),
+        // ...mapActions(['addOdd','addWait']),//数组的时候要注意改上边的名字
+        //mapMutations会调用commit联系mutations
+        ...mapMutations({addClick:'ADD',reduceClick:'REDUCE'}),
+        // ...mapMutations(['ADD','REDUCE'])//数组的时候要注意改上边的名字
     },
 }
 </script>
